5d257d82df2cb01083a305301974c60f22b88b0f
salimkhandev <147515973+salimkhandev@users.noreply.github.com>
2024-07-11 23:31:27 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ function App() {
 
   return (
     <div className="App">
+      <img src="/src/assets/logo.jpg" alt="" srcset="" />
       <Header />
       <main className="pt-13">
         <Home />
