12b62f7e83caa9301e251f3a6de81cdd2933ebd6
salimkhandev <147515973+salimkhandev@users.noreply.github.com>
2025-03-28 22:11:41 +0500

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -17,7 +17,7 @@ const Project = (props) => {
         rel="noopener noreferrer"
         className="text-blue-500 hover:underline flex items-center justify-center"
       >
-        View Project
+        Visit Project
         <FontAwesomeIcon icon={faExternalLinkAlt} style={{ marginLeft: '8px' }} />
       </a>
     </div>
